669837ff076b65477cfc8688c0b11ef2235e00f0
Sohyeon Park <thgus4618@naver.com>
2017-08-24 09:21:34 +0900

    Modify code

--- FireAlarm/app/src/main/java/dsm/firealarm/MainActivity.java
+++ FireAlarm/app/src/main/java/dsm/firealarm/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         TextView message = (TextView) findViewById(R.id.message);
         message.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.test,0,0,0);
+
+/**푸시메시지 부분**/
 //        message.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -59,36 +60,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 관리자만 사용할 수 있는 기능 비활성화 개발해야 함.
 
-//
-//        btnTest = (Button) findViewById(R.id.btnTest);
-//        btnTest.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), TestActivity.class);
-//                startActivityForResult(intent, 1000);
-//            }
-//        });
-
-        // 관리자만 사용할 수 있는 기능 비활성화
-/*
-        btnSending = (Button) findViewById(R.id.btnSending);
-        btnSending.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Sending.class 생성해야 함);
-                startActivityForResult(intent, 1000);
-            }
-        });
-*/
-
-//        btnSettings = (Button) findViewById(R.id.btnSettings);
-//        btnSettings.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
-//                startActivityForResult(intent, 1000);
-//            }
-//        });
     }
 }
