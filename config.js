568b7c96b023266e307c0782c77dadcfb8d47453
syeutyu <syeuty@naver.com>
2017-09-04 11:35:26 +0900

    config

--- config.js
+++ config.js
@@ -3,17 +3,12 @@ let config = {
     server_port: 3000,
     db_url: 'mongodb://dong:kyera12589@ds137261.mlab.com:37261/dayzen',
     db_schemas: [{
-            "file": './Lasbery/lasberyModel',
-            "modelName": 'lasbery'
-        }, {
-            "file": "./Android/androidModel",
-            "modelName": 'android'
-        },
-        {
-            "file": "./Test/testModel",
-            "modelName": 'test'
-        }
-    ]
+        "file": './Lasbery/lasberyModel',
+        "modelName": 'lasbery'
+    }, {
+        "file": "./Android/androidModel",
+        "modelName": 'android'
+    }]
 }
 
 module.exports = config;
