316618b807ac8d54d6de07c96815dc40787824d8
syeutyu <syeuty@naver.com>
2017-09-04 11:34:26 +0900

    push

--- Mongo/Test/Schema.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-
-let schema = mongoose.Schema({
-    R_num : {type : String, unique : true},
-    check : Boolean
-}, { collection: 'Record'});
-
-module.exports = schema;
