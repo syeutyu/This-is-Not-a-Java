396f943ec0fe4a3489113f54de0bd24e633a41dc
syeutyu <syeuty@naver.com>
2017-09-25 11:33:18 +0900

    기본 url

--- route/androidRouter/router.js
+++ route/androidRouter/router.js
@@ -3,6 +3,9 @@ let logic = require('./aController');
 let middle = require('../../middle/sessionCheck');
 let fcm = require('../fcm');
 
+router.route('/').get((req, res) => {
+    console.log('test');
+});
 router.route('/auth/signin').post(logic.splash);
 router.route('/auth/signup').post(logic.signup);
 router.route('/auth/test').get(middle, logic.test).post(middle, logic.test);
