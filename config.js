0b5ed1b60817da936e7d5f69be11963e65280b58
syeutyu <syeuty@naver.com>
2017-09-25 10:41:35 +0900

    config DB_url

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
 
     server_port: 3000,
-    // db_url: 'mongodb://dong:kyera12589@ds137261.mlab.com:37261/dayzen',
-    db_url: 'localhost:27017/test'
+    db_url: 'mongodb://dong:kyera12589@ds137261.mlab.com:37261/dayzen',
+    //db_url: 'localhost:27017/test'
 };
 
 module.exports = config;
