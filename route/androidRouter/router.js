2bcefbe0319b74898344b261ecbc8242cab0b8ea
syeutyu <syeuty@naver.com>
2017-09-26 20:11:27 +0900

    test

--- route/androidRouter/router.js
+++ route/androidRouter/router.js
@@ -5,9 +5,12 @@ let fcm = require('../fcm');
 
 router.route('/').get((req, res) => {
     console.log('test');
+}).post((req, res) => {
+    console.log('teset');
 });
 router.route('/auth/signin').post(logic.splash);
 router.route('/auth/signup').post(logic.signup);
 router.route('/auth/test').get(middle, logic.test).post(middle, logic.test);
 router.route('/auth/search').post(middle, logic.search);
+
 module.exports = router;
