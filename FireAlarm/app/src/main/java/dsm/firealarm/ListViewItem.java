6a593c98050358479093d02590242e2de7519b72
Sohyeon <thgus4618@naver.com>
2017-10-09 11:59:15 +0900

    Develop Filtering

---
+++ FireAlarm/app/src/main/java/dsm/firealarm/ListViewItem.java
@@ -0,0 +1,43 @@
+package dsm.firealarm;
+
+import android.graphics.drawable.Drawable;
+
+import java.text.Collator;
+import java.util.Comparator;
+
+/**
+ * Created by 10107PSH on 2017-08-28.
+ */
+
+public class ListViewItem {
+    /**
+     * 리스트 정보를 담고 있을 객체 생성
+     */
+    public String nameStr; // 유저 네임
+    public String spotStr; // 화재 발생 장소
+    public String timeStr; // 화재 발생 시각
+
+    public String getNameStr() {
+        return nameStr;
+    }
+
+    public void setNameStr(String nameStr) {
+        this.nameStr = nameStr;
+    }
+
+    public String getSpotStr() {
+        return spotStr;
+    }
+
+    public void setSpotStr(String spotStr) {
+        this.spotStr = spotStr;
+    }
+
+    public String getTimeStr() {
+        return timeStr;
+    }
+
+    public void setTimeStr(String timeStr) {
+        this.timeStr = timeStr;
+    }
+}
--- FireAlarm/app/src/main/java/dsm/firealarm/RecordActivity.java
+++ FireAlarm/app/src/main/java/dsm/firealarm/RecordActivity.java
@@ -1,115 +1,144 @@
 package dsm.firealarm;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class RecordActivity extends AppCompatActivity {
 
-    private ListView mListView = null;
-    private ListViewAdapter mAdapter = null;
+    ListView listview = null;
 
-    private class ViewHolder {
-        public ImageView mTestIcon;
+    private class ListViewAdapter extends BaseAdapter implements Filterable {
+        // Adapter에 추가된 데이터를 저장하기 위한 ArrayList. (원본 데이터 리스트)
+        private ArrayList<ListViewItem> listViewItemList = new ArrayList<>();
+        // 필터링된 결과 데이터를 저장하기 위한 ArrayList. 최초에는 전체 리스트 보유.
+        private ArrayList<ListViewItem> filteredItemList = listViewItemList;
 
-        public TextView mUserName;
+        Filter listFilter;
 
-        public TextView mSpot;
-
-        public TextView mTime;
-    }
-
-    private class ListViewAdapter extends BaseAdapter {
-        private Context mContext = null;
-        private ArrayList<ListData> mListData = new ArrayList<>();
-
-        public ListViewAdapter(Context mContext) {
-            super();
-            this.mContext = mContext;
+        // ListViewAdapter의 생성자
+        public ListViewAdapter() {
         }
 
+        // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
         @Override
         public int getCount() {
-            return mListData.size();
+            return filteredItemList.size();
         }
 
         @Override
-        public Object getItem(int position) {
-            return mListData.get(position);
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final Context context = parent.getContext();
+
+            // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.listview_item, parent, false);
+            }
+
+            // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+            TextView nameStr = (TextView) convertView.findViewById(R.id.nameStr);
+            TextView spotStr = (TextView) convertView.findViewById(R.id.spotStr);
+            TextView timeStr = (TextView) convertView.findViewById(R.id.timeStr);
+
+            // Data Set(filteredItemList)에서 position에 위치한 데이터 참조 획득
+            ListViewItem listViewItem = filteredItemList.get(position);
+
+            // 아이템 내 각 위젯에 데이터 반영
+            nameStr.setText(listViewItem.getNameStr());
+            spotStr.setText(listViewItem.getSpotStr());
+            timeStr.setText(listViewItem.getTimeStr());
+
+            return convertView;
         }
 
+        // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
         @Override
         public long getItemId(int position) {
             return position;
         }
 
+        // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                holder = new ViewHolder();
+        public Object getItem(int position) {
+            return filteredItemList.get(position);
+        }
 
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.listview_item, null);
+        // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
 
-                holder.mTestIcon = (ImageView) convertView.findViewById(R.id.mTestImage);
-                holder.mUserName = (TextView) convertView.findViewById(R.id.mUserName);
-                holder.mSpot = (TextView) convertView.findViewById(R.id.mSpot);
-                holder.mTime = (TextView) convertView.findViewById(R.id.mTime);
+        public void addItem(String nameStr, String spotStr, String timeStr) {
+            ListViewItem item = new ListViewItem();
+            item.setNameStr(nameStr);
+            item.setSpotStr(spotStr);
+            item.setTimeStr(timeStr);
 
-                convertView.setTag(holder);
-            }else{
-                holder = (ViewHolder) convertView.getTag();
+            listViewItemList.add(item);
+        }
+
+        // TODO : filtering item.
+
+        @Override
+        public Filter getFilter() {
+            if (listFilter == null) {
+                listFilter = new ListFilter();
+            }
+            return listFilter;
+        }
+
+        private class ListFilter extends Filter {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+
+                if (constraint == null || constraint.length() == 0) {
+                    results.values = listViewItemList;
+                    results.count = listViewItemList.size();
+                } else {
+                    ArrayList<ListViewItem> itemList = new ArrayList<>();
+
+                    for (ListViewItem item : listViewItemList) {
+                        if (item.getNameStr().toUpperCase().contains(constraint.toString().toUpperCase()) ||
+                                item.getSpotStr().toUpperCase().contains(constraint.toString().toUpperCase()) ||
+                                item.getTimeStr().toUpperCase().contains(constraint.toString().toUpperCase())) {
+                            itemList.add(item);
+                        }
+                    }
+
+                    results.values = itemList;
+                    results.count = itemList.size();
+                }
+                return results;
             }
 
-            ListData mData = mListData.get(position);
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
 
-            if (mData.mTestIcon != null) {
-                holder.mTestIcon.setVisibility(View.VISIBLE);
-                holder.mTestIcon.setImageDrawable(mData.mTestIcon);
-            }else{
-                holder.mTestIcon.setVisibility(View.GONE);
+                // update listview by filtered data list.
+                filteredItemList = (ArrayList<ListViewItem>) results.values;
+
+                // notify
+                if (results.count > 0) {
+                    notifyDataSetChanged();
+                } else {
+                    notifyDataSetInvalidated();
+                }
             }
-
-            holder.mUserName.setText(mData.mName);
-            holder.mSpot.setText(mData.mSpot);
-            holder.mTime = (TextView) convertView.findViewById(R.id.mTime);
-
-            return convertView;
         }
 
-        public void addItem(Drawable icon, String mName, String mSpot, String mTime){
-            ListData addInfo = null;
-            addInfo = new ListData();
-            addInfo.mTestIcon = icon;
-            addInfo.mName = mName;
-            addInfo.mSpot = mSpot;
-            addInfo.mTime = mTime;
-
-            mListData.add(addInfo);
-        }
-
-        public void sort(){
-            Collections.sort(mListData, ListData.ALPHA_COMPARATOR);
-        }
-
-        public void fillter(String searchText) {
-
-        }
 
     }
 
@@ -118,34 +147,46 @@ public class RecordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
-        mListView = (ListView) findViewById(R.id.mList);
+        ListViewAdapter adapter;
 
-        mAdapter = new ListViewAdapter(this);
-        mListView.setAdapter(mAdapter);
+        // Adapter 생성
+        adapter = new ListViewAdapter();
 
-        mAdapter.addItem(getResources().getDrawable(R.drawable.history),
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
+        adapter.addItem(
                 "박소현",
                 "화재 발생 장소 1",
                 "2014-02-18");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.history),
+        adapter.addItem(
                 "이동현",
                 "화재 발생 장소 2",
                 "2014-02-01");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.history),
+        adapter.addItem(
                 "김경민",
                 "화재 발생 장소 3",
                 "2014-02-04");
-        mAdapter.addItem(getResources().getDrawable(R.drawable.history),
+        adapter.addItem(
                 "나호겸",
                 "화재 발생 장소 4",
                 "2014-02-15");
 
-        mListView.setOnItemClickListener(new OnItemClickListener() {
+        EditText editTextFilter = (EditText) findViewById(R.id.editTextFilter);
+        editTextFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable edit) {
+                String filterText = edit.toString();
+                ((ListViewAdapter) listview.getAdapter()).getFilter().filter(filterText);
+            }
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id){
-                ListData mData = mAdapter.mListData.get(position);
-                Toast.makeText(RecordActivity.this, mData.mName, Toast.LENGTH_SHORT).show();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
         });
     }
