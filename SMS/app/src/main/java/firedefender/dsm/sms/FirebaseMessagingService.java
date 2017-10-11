b9f78d6c7e7b7059f3c0df45cc835d0853e1fe8c
Sohyeon <thgus4618@naver.com>
2017-10-11 10:27:39 +0900

    Develop SMS

---
+++ SMS/app/src/main/java/firedefender/dsm/sms/FirebaseMessagingService.java
@@ -0,0 +1,68 @@
+package firedefender.dsm.sms;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sohyeon Park on 2017-09-05.
+ * 푸시메세지가 들어왔을때 실제 사용자에게 푸시알림을 만들어서 띄워주는 클래스.
+ * API를 통해 푸시 알림을 전송하면 입력한 내용이 message에 담겨서 오게 됨.
+ */
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    private static final String TAG = "FirebaseMsgService";
+    private String msg;
+
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("xxx",remoteMessage.getData().get("text"));
+
+        String text = remoteMessage.getData().get("text");
+        Intent intentText = new Intent(getApplicationContext(),MainActivity.class);
+        intentText.putExtra("text",text);
+
+        String tels = remoteMessage.getData().get("tels");
+        Intent intentTels = new Intent(getApplicationContext(),MainActivity.class);
+        intentTels.putExtra("tels",tels);
+
+        // TODO response.tels[0]
+        Log.d("ooo",remoteMessage.getData().get("tels[0]"));
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Mesage data payload: " + remoteMessage.getData());
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Mesage Notification Body: " + remoteMessage.getNotification());
+        }
+
+        msg = remoteMessage.getNotification().getBody();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
+//                .setContentText(msg)
+//                .setAutoCancel(true)
+//                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+//                .setVibrate(new long[]{1, 1000});
+
+//        NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        notificationManager.notify(0/* ID of notification */, mBuilder.build());
+//
+//        mBuilder.setContentIntent(contentIntent);
+    }
+}
+
