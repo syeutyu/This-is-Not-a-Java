394355cfc12a684ebb3cc1a7218f884a879252fe
Sohyeon <thgus4618@naver.com>
2017-10-13 16:14:23 +0900

    Develop FCM

---
+++ AlarmSMS/app/src/main/java/dsm/alarmsms/FirebaseInstanceIDService.java
@@ -0,0 +1,24 @@
+package dsm.alarmsms;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Sohyeon Park on 2017-09-05.
+ * 사용자 기기별 token을 생성하는 클래스.
+ * 나중에 push 알림을 특정 타겟에게 보낼 때 사용되는 고유 키 값.
+ * 토큰값을 용도에 맞게 사용
+ */
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseIIDService";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        Log.d(TAG, FirebaseInstanceId.getInstance().getToken());
+    }
+}
---
+++ AlarmSMS/app/src/main/java/dsm/alarmsms/MainActivity.java
@@ -0,0 +1,19 @@
+package dsm.alarmsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d("토큰",token);
+    }
+}
