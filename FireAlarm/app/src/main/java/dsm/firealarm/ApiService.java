a4d44056e3bdcb60591cfd42fa232619f7fca13c
Sohyeon <thgus4618@naver.com>
2017-10-21 03:39:22 +0900

    Modify Api Service

--- FireAlarm/app/src/main/java/dsm/firealarm/ApiService.java
+++ FireAlarm/app/src/main/java/dsm/firealarm/ApiService.java
@@ -1,5 +1,7 @@
 package dsm.firealarm;
 
+import com.google.gson.JsonObject;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -10,12 +12,21 @@ import retrofit2.http.POST;
  */
 
 public interface ApiService {
-
     public static final String API_URL = "https://daejava.herokuapp.com/";
 
     @FormUrlEncoded
-    @POST("signin")
+    @POST("auth/signin")
     Call<Void> signin(@Field("token") String token);
 
+    @FormUrlEncoded
+    @POST("auth/signup")
+    Call<Void> signup (@Field("name") String name, @Field("code") String code, @Field("place") String place, @Field("token") String token, @Field("tel")String tel);
 
+    @FormUrlEncoded
+    @POST("auth/search")
+    Call<JsonObject> search (@Field("token")String token);
+
+    @FormUrlEncoded
+    @POST("/auth/test")
+    Call<Void> test (@Field("bool") Boolean bool, @Field("token") String key);
 }
